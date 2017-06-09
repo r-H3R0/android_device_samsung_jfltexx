2d2bbb3b41b7797a10a4e644bc19c6a7a79f5f3d
BenLue <benlue@s3root.ovh>
2017-06-09 14:58:26 +0200

    Jfltexx: Add Device

---
+++ xenonhd.mk
@@ -0,0 +1,46 @@
+# Release name
+PRODUCT_RELEASE_NAME := jfltexx
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device
+$(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := 
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := jfltexx
+PRODUCT_NAME := xenonhd_jfltexx
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-I9505
+
+# Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer="BenLue"
+TARGET_VENDOR := 
+
+# Root options
+ROOT_METHOD=su
+# Without any of these flags the builds will be unrooted and won't include Adaway & KernelAdiutor
+
+# 3rd party apps - choose which you want to build
+PRODUCT_PACKAGES += \
+    Adaway \
+    KernelAdiutor \
+    MiXplorer
+    
+# Camera
+PRODUCT_PACKAGES += \
+  Snap
+
+# Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltexx \
+  TARGET_DEVICE=jfltexx \
+  BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHPF4:user/release-keys \
+  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
+endif
